0ae0cfbfb824749d174e523e226ad59070db41b7
Simon Wetting <swetting@f0r3s19.codam.nl>
2026-05-06 13:23:46 +0200

    push

--- M03/ex4/ft_inventory_system.py
+++ M03/ex4/ft_inventory_system.py
@@ -3,6 +3,7 @@ import sys
 def main() -> None:
 	args = sys.argv[1:]
 	inventory: list[tuple[str, int]] = []
+	distinct_items = 0
 	for arg in args:
 		if ":" not in arg:
 			print("skipping arg without :")
@@ -10,34 +11,17 @@ def main() -> None:
 		tmp = arg.split(":", 1)
 		try:
 			item: tuple[str, int] = (tmp[0], int(tmp[1]))
-		except:
-			print(f"{tmp[0]} has an invalid value")
+		except ValueError as e:
+			print(f"{tmp[0]} {e}")
+		else:
+			distinct_items += 1
+		if inventory.fin
 		inventory.append(item)
+	print("=== Your Inventory ===")
+	total: int = sum(quantity for name, quantity in inventory)
+	print(f"Total quantity of the {distinct_items} items: {total}")
 	for name, quantity in inventory:
-		print(f"{quantity} times {name}")
-
-# import sys
-
-# def main() -> None:
-#     args = sys.argv[1:]
-#     inventory: list[tuple[str, int]] = []
-
-#     for arg in args:
-#         if ":" not in arg:
-#             print(f"Skipping invalid: {arg}")
-#             continue
-#         tmp = arg.split(":", 1)
-#         name = tmp[0].strip()
-#         try:
-#             quantity = int(tmp[1].strip())
-#             inventory.append((name, quantity))
-#         except ValueError:
-#             print(f"Skipping invalid quantity: {arg}")
-
-#     print("=== Your Inventory ===")
-#     for name, quantity in inventory:
-#         print(f"{quantity} times {name}")
-
+		print(f"Item {name} represents {round(float(100 * quantity/total), 3)}%")
 
 if __name__ == "__main__":
     main()
