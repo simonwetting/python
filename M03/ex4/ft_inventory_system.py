2e740c5ba1b1817e8fb2c4b77c58f447d45cc328
Simon Wetting <swetting@f1r4s6.codam.nl>
2026-05-07 12:00:20 +0200

    push

--- M03/ex4/ft_inventory_system.py
+++ M03/ex4/ft_inventory_system.py
@@ -1,27 +1,54 @@
 import sys
 
+
 def main() -> None:
-	args = sys.argv[1:]
-	inventory: list[tuple[str, int]] = []
-	distinct_items = 0
-	for arg in args:
-		if ":" not in arg:
-			print("skipping arg without :")
-			continue
-		tmp = arg.split(":", 1)
-		try:
-			item: tuple[str, int] = (tmp[0], int(tmp[1]))
-		except ValueError as e:
-			print(f"{tmp[0]} {e}")
-		else:
-			distinct_items += 1
-		if inventory.fin
-		inventory.append(item)
-	print("=== Your Inventory ===")
-	total: int = sum(quantity for name, quantity in inventory)
-	print(f"Total quantity of the {distinct_items} items: {total}")
-	for name, quantity in inventory:
-		print(f"Item {name} represents {round(float(100 * quantity/total), 3)}%")
+    args = sys.argv[1:]
+    inventory: dict[str, int] = {}
+    distinct_items = 0
+    for arg in args:
+        if ":" not in arg:
+            print("skipping arg without :")
+            continue
+        tmp = arg.split(":", 1)
+        try:
+            item: tuple[str, int] = (tmp[0], int(tmp[1]))
+        except ValueError as e:
+            print(f"{tmp[0]} {e}")
+        else:
+            distinct_items += 1
+        exists = item in inventory
+        if not exists:
+            inventory[item[0]] = item[1]
+        else:
+            inventory.get(item[0]) + item[1]
+    print("=== Your Inventory ===")
+    total: int = sum(quantity for name, quantity in inventory)
+    print(f"Total quantity of the {distinct_items} items: {total}")
+    for name, quantity in inventory:
+        print(f"Item {name} represents {round(float(100 * quantity/total), 3)}%")
+
+# def main() -> None:
+# args = sys.argv[1:]
+# inventory: list[tuple[str, int]] = []
+# distinct_items = 0
+# for arg in args:
+# if ":" not in arg:
+# print("skipping arg without :")
+# continue
+# tmp = arg.split(":", 1)
+# try:
+# item: tuple[str, int] = (tmp[0], int(tmp[1]))
+# except ValueError as e:
+# print(f"{tmp[0]} {e}")
+# else:
+# distinct_items += 1
+# inventory.append(item)
+# print("=== Your Inventory ===")
+# total: int = sum(quantity for name, quantity in inventory)
+# print(f"Total quantity of the {distinct_items} items: {total}")
+# for name, quantity in inventory:
+# print(f"Item {name} represents {round(float(100 * quantity/total), 3)}%")
+
 
 if __name__ == "__main__":
     main()
